25f5e6571247fdb96f1b1d8fa20fac8a3c4087a6
kurbakov <kurbakov.d@gmail.com>
2018-10-10 23:55:39 +0200

    remove unused function and code formating

--- source/util/files.py
+++ source/util/files.py
@@ -3,52 +3,15 @@ Utilities to manipulate files and directories.
 """
 
 import glob
-import zipfile
 import os
-import time
 
-def most_recent_file(dir_path, ext=''):
+
+def expand_path_mask(path: str) -> list:
     """
-    return the most recent file given a directory path and extension
+    ToDo
+    :param path: Todo
+    :return: ToDo
     """
-    query = dir_path + '/*' + ext
-    newest = min(glob.iglob(query), key=os.path.getctime)
-    return newest
-
-
-def make_dir(path):
-    real_path = os.path.expanduser(path)
-    if not os.path.exists(real_path):
-        os.makedirs(real_path)
-    return real_path
-
-
-def zip_dir(dir_path, zip_path):
-    """
-    Create and save a zipfile of a one level directory
-    """
-    file_paths = glob.glob(dir_path + "/*")  # create path to search for files.
-
-    zf = zipfile.ZipFile(zip_path, 'w')
-    dir_name = os.path.basename(dir_path)
-    for p in file_paths:
-        file_name = os.path.basename(p)
-        zf.write(p, arcname=os.path.join(dir_name, file_name))
-    zf.close()
-    return zip_path
-
-
-def time_since_last_file_edited(path):
-    """return seconds since last file was updated"""
-    list_of_files = glob.glob(os.path.join(path, '*'))
-    if len(list_of_files) > 0:
-        latest_file = max(list_of_files, key=os.path.getctime)
-        return int(time.time() - os.path.getctime(latest_file))
-    return 0
-
-
-
-def expand_path_mask(path):
     matches = []
     path = os.path.expanduser(path)
     for file in glob.glob(path):
@@ -57,7 +20,12 @@ def expand_path_mask(path):
     return matches
 
 
-def expand_path_arg(path_str):
+def expand_path_arg(path_str: str) -> list:
+    """
+    ToDo
+    :param path_str: ToDo
+    :return: ToDo
+    """
     path_list = path_str.split(",")
     expanded_paths = []
     for path in path_list:
