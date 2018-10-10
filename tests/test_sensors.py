9ed8a63d06531b4012864be1b69db2c7fd849e8a
kurbakov <kurbakov.d@gmail.com>
2018-10-10 23:39:55 +0200

    make refactoring of camera

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -12,9 +12,17 @@ def test_base_camera():
 @pytest.mark.skipif(on_pi() == False, reason='only works on RPi')
 def test_picamera():
     from source.parts.camera import PiCamera
-    resolution = (120,160)
-    cam = PiCamera(resolution=resolution)
+    cfg = object(
+        CAMERA_RESOLUTION_HIGH=120,
+        CAMERA_RESOLUTION_WIDTH = 160,
+        CAMERA_FRAMERATE = 20,
+        CAMERA_ZOOM = (0, 0, 1, 1),
+        CAMERA_COLORBALANCE_LENS = 0,
+        CAMERA_COLORBALANCE_RGB = None,
+        CAMERA_COLORBALANCE_UV = None
+    )
+    cam = PiCamera(cfg)
     frame = cam.run()
     #assert shape is as expected. img_array shape shows (width, height, channels)
-    assert frame.shape[:2] == resolution[:]
+    assert frame.shape[:2] == [cfg.CAMERA_RESOLUTION_HIGH, cfg.CAMERA_RESOLUTION_WIDTH]
 
