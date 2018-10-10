f94130a4133bfea43c5e219ab3ea4c61e93540cc
kurbakov <kurbakov.d@gmail.com>
2018-10-11 00:12:29 +0200

    add comments

--- source/util/database.py
+++ source/util/database.py
@@ -2,12 +2,26 @@ import sqlite3
 
 
 class Database:
-	def __init__(self, path):
+	def __init__(self, path: str, col_names: list, col_data_types: list) -> None:
+		"""
+		Constructor
+		:param path: Path to the SQLite database
+		:param col_names: Names of the columns
+		:param col_data_types: Data types of the columns
+		:return: Void function
+		"""
 		self.connection = sqlite3.connect(path)
 		self.cursor = self.conn.cursor()
-		self.__create_table("main", ["image", "throttling", "angle", "time"], ["blob", "double", "double", "datetime"])
+		self.__create_table("main", col_names + ["timestamp"], col_data_types + ["datetime"])
 
-	def __create_table(self, table_name, column_names, column_types):
+	def __create_table(self, table_name: str, column_names: list, column_types: list) -> None:
+		"""
+		Create the table in the SQLite database
+		:param table_name: name of the table
+		:param column_names: columns names
+		:param column_types: data types of the columns
+		:return:
+		"""
 		if len(column_names) != len(column_types):
 			raise()
 
@@ -23,11 +37,22 @@ class Database:
 		finally:
 			self.close()
 
-	def save_record(self, image, throttling, turn_angle):
+	def save_record(self, image: object, throttling: float, turn_angle: float) -> None:
+		"""
+		Save the image and the values from the controller
+		:param image: Image
+		:param throttling: The value of the throttling
+		:param turn_angle: The value of the turn angle
+		:return: Void function
+		"""
 		sql_query = "INSERT INTO main VALUES(?, ?, ?, datetime('now');"
 		self.cursor.execute(sql_query, [sqlite3.Binary(image), throttling, turn_angle])
 		self.connection.commit()
 
-	def close(self):
+	def close(self) -> None:
+		"""
+		Close the connection to the database
+		:return: Void function
+		"""
 		self.connection.close()
 
