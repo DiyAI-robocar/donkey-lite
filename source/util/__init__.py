b47c618732028e20b45f4fbc4b188c6cb315928f
kurbakov <kurbakov.d@gmail.com>
2018-10-14 16:37:06 +0200

    remove duplications

--- source/util/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import proc
-from . import data
-from . import files
-from . import img
-from . import web
--- tests/setup.py
+++
@@ -1,7 +0,0 @@
-import platform
-
-
-def on_pi():
-    if 'arm' in platform.machine():
-        return True
-    return False
